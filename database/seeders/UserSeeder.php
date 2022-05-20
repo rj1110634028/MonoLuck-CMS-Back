2793de1a1557cb7f545614f06586cead10f1d866
Jonathan <jo901025@gmail.com>
2022-05-20 09:45:11 +0800

    seeder models name to upper

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use \App\Models\user;
+use \App\Models\User;
 use \App\Models\locker;
 use Illuminate\Support\Facades\Hash;
 
@@ -18,28 +18,28 @@ class UserSeeder extends Seeder
     public function run()
     {
 
-        // user::factory(20)->create()->each(function ($u) {
+        // User::factory(20)->create()->each(function ($u) {
         //     locker::insert([]);
-        //     $locker = locker::where('userId', '=', NULL)->where('lockerNo', '<>', NULL)->inRandomOrder()->first();
+        //     $locker = locker::where('UserId', '=', NULL)->where('lockerNo', '<>', NULL)->inRandomOrder()->first();
         //     if ($locker != NULL) {
-        //         $locker->update(['userId' => $u->id]);
+        //         $locker->update(['UserId' => $u->id]);
         //     }
         // });
         
         //create test RootUser
-        user::create([
+        User::create([
             'permission' => 0,
             'email' => '000@example.com',
             'name' => 'root',
             'password' => Hash::make('root'),
         ]);
-        user::create([
+        User::create([
             'permission' => 0,
             'email' => '002@example.com',
             'name' => 'root2',
             'password' => Hash::make('root'),
         ]);
-        user::create([
+        User::create([
             'permission' => 0,
             'email' => '003@example.com',
             'name' => 'root3',
