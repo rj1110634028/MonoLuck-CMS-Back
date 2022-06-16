07a0a1eafcbc4d39db5228a5e0642bca8b340c7a
Jonathan <jo901025gmail.com>
2022-06-16 11:22:59 +0800

    update seeders

--- database/seeders/RootUserSeeder.php
+++ database/seeders/RootUserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use \App\Models\User;
-use \App\Models\locker;
+use \App\Models\Locker;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
