c4b14622ef624e7bf92a35a4986d3daa0f616843
Jonathan <jo901025@gmail.com>
2022-06-27 10:22:39 +0800

    try test

--- tests/Feature/APITest.php
+++ tests/Feature/APITest.php
@@ -17,28 +17,28 @@ class APITest extends TestCase
      */
     public function test_the_login_returns_a_successful_response()
     {
-        $response = $this->post('/api/login', ['mail'=>'003@example.com', 'password'=>'root']);
+        $response = $this->post('/api/login', ['mail' => '003@example.com', 'password' => 'root']);
         $response->assertStatus(200);
         $this::$token = $response->getData()->message->token;
     }
 
     public function test_the_getLocker_returns_a_successful_response()
     {
-        $response = $this->withHeaders(["token"=>$this::$token])->get('/api/locker');
+        $response = $this->withHeaders(["token" => $this::$token])->get('/api/locker');
         $response->assertStatus(200);
     }
     public function test_the_unLock_returns_a_successful_response()
     {
-        $response = $this->withHeaders(["token"=>$this::$token])->post('/api/unlock',[
-            "lockerNo"=>"00",
-            "description"=>"test"
+        $response = $this->withHeaders(["token" => $this::$token])->post('/api/unlock', [
+            "lockerNo" => "00",
+            "description" => "test"
         ]);
         $response->assertStatus(200);
     }
 
     public function test_the_addUser_returns_a_successful_response()
     {
-        $response = $this->withHeaders(["token"=>$this::$token])->post('/api/user',[
+        $response = $this->withHeaders(["token" => $this::$token])->post('/api/user', [
             "lockerNo" => "00",
             'name' => "test",
             'phone' => "0910237485",
@@ -51,14 +51,14 @@ class APITest extends TestCase
 
     public function test_the_getRecord_returns_a_successful_response()
     {
-        $response = $this->withHeaders(["token"=>$this::$token])->get('/api/record/00');
+        $response = $this->withHeaders(["token" => $this::$token])->get('/api/record/00');
         $response->assertStatus(200);
     }
 
 
     public function test_the_updateUser_returns_a_successful_response()
     {
-        $response = $this->withHeaders(["token"=>$this::$token])->patch('/api/user/'.$this::$userId,[
+        $response = $this->withHeaders(["token" => $this::$token])->patch('/api/user/' . $this::$userId, [
             'name' => "test2",
             'phone' => "0910237487",
             'mail' => "test2@example.com",
@@ -69,13 +69,13 @@ class APITest extends TestCase
 
     public function test_the_deleteUser_returns_a_successful_response()
     {
-        $response = $this->withHeaders(["token"=>$this::$token])->delete('/api/user/'.$this::$userId);
+        $response = $this->withHeaders(["token" => $this::$token])->delete('/api/user/' . $this::$userId);
         $response->assertStatus(200);
     }
 
     public function test_the_logout_returns_a_successful_response()
     {
-        $response = $this->withHeaders(["token"=>$this::$token])->get('/api/logout');
+        $response = $this->withHeaders(["token" => $this::$token])->get('/api/logout');
         $response->assertStatus(200);
     }
 }
