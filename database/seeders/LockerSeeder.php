5518d0e30e385ef13f2a051fd0a6dc8aa671275f
Jonathan <jo901025@gmail.com>
2022-08-04 14:26:22 +0800

    fix sonarcloud issue

--- database/seeders/LockerSeeder.php
+++ database/seeders/LockerSeeder.php
@@ -17,142 +17,16 @@ class LockerSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        $lockerList = [
-            [
-                'lockerNo' => "00",
-            ],
-            [
-                'lockerNo' => "01",
-            ],
-            [
-                'lockerNo' => NULL,
-            ],
-            [
-                'lockerNo' => NULL,
-            ],
-            [
-                'lockerNo' => NULL,
-            ],
-            [
-                'lockerNo' => NULL,
-            ],
-            [
-                'lockerNo' => "02",
-            ],
-            [
-                'lockerNo' => "03",
-            ],
-            [
-                'lockerNo' => "04",
-            ],
-            [
-                'lockerNo' => NULL,
-            ],
-            [
-                'lockerNo' => NULL,
-            ],
-            [
-                'lockerNo' => NULL,
-            ],
-            [
-                'lockerNo' => NULL,
-            ],
-            [
-                'lockerNo' => "05",
-            ],
-            [
-                'lockerNo' => "06",
-            ],
-            [
-                'lockerNo' => "07",
-            ],
-            [
-                'lockerNo' => "08",
-            ],
-            [
-                'lockerNo' => "09",
-            ],
-            [
-                'lockerNo' => "10",
-            ],
-            [
-                'lockerNo' => "11",
-            ],
-            [
-                'lockerNo' => "12",
-            ],
-            [
-                'lockerNo' => "13",
-            ],
-            [
-                'lockerNo' => "14",
-            ],
-            [
-                'lockerNo' => "15",
-            ],
-            [
-                'lockerNo' => "16",
-            ],
-            [
-                'lockerNo' => "17",
-            ],
-            [
-                'lockerNo' => "18",
-            ],
-            [
-                'lockerNo' => "19",
-            ],
-            [
-                'lockerNo' => "20",
-            ],
-            [
-                'lockerNo' => "21",
-            ],
-            [
-                'lockerNo' => "22",
-            ],
-            [
-                'lockerNo' => "23",
-            ],
-            [
-                'lockerNo' => "24",
-            ],
-            [
-                'lockerNo' => "25",
-            ],
-            [
-                'lockerNo' => "26",
-            ],
-            [
-                'lockerNo' => "27",
-            ],
-            [
-                'lockerNo' => "28",
-            ],
-            [
-                'lockerNo' => "29",
-            ],
-            [
-                'lockerNo' => "30",
-            ],
-            [
-                'lockerNo' => "31",
-            ],
-            [
-                'lockerNo' => "32",
-            ],
-            [
-                'lockerNo' => "33",
-            ],
-        ];
+        $lockerList = ["00", "01", NULL, NULL, NULL, NULL, "02", "03", "04", NULL, NULL, NULL, NULL, "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33",];
         foreach ($lockerList as $value) {
-            if ($value['lockerNo'] != NULL) {
-                $value['lockerEncoding'] = $faker->unique()->regexify('[0-9]{4}');
+            $locker = ['lockerNo' => $value];
+            if ($value != NULL) {
+                $locker['lockerEncoding'] = $faker->unique()->regexify('[0-9]{4}');
             } else {
-                $value['lockerEncoding'] = NULL;
+                $locker['lockerEncoding'] = NULL;
             }
             Locker::create(
-                $value
+                $locker
             );
         }
     }
